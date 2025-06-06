f4f48a7521658dfd9c8ad0a44168150b079a1543
2crdl <sjalvmord@bk.ru>
2025-06-06 15:53:33 +0300

    Persist users to file

--- db.go
+++ db.go
@@ -16,11 +16,11 @@ const (
 // Для pending_names.txt: id:name:username:phone
 
 type UserRecord struct {
-	ID      int64
-	Name    string
-	Role    string // только для known_users
+	ID       int64
+	Name     string
+	Role     string // только для known_users
 	Username string
-	Phone   string
+	Phone    string
 }
 
 // --- KNOWN USERS ---
@@ -35,13 +35,9 @@ func IsKnownUser(chatID int64) bool {
 }
 
 func AddKnownUserFull(user UserRecord) error {
-	f, err := os.OpenFile(KnownUsersFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	_, err = f.WriteString(fmt.Sprintf("%d:%s:%s:%s:%s\n", user.ID, user.Name, user.Role, user.Username, user.Phone))
-	return err
+	users, _ := LoadKnownUsers()
+	users[user.ID] = user
+	return SaveAllKnownUsers(users)
 }
 
 func RemoveKnownUser(chatID int64) error {
@@ -76,11 +72,11 @@ func LoadKnownUsers() (map[int64]UserRecord, error) {
 		var id int64
 		fmt.Sscanf(parts[0], "%d", &id)
 		users[id] = UserRecord{
-			ID:      id,
-			Name:    parts[1],
-			Role:    parts[2],
+			ID:       id,
+			Name:     parts[1],
+			Role:     parts[2],
 			Username: parts[3],
-			Phone:   parts[4],
+			Phone:    parts[4],
 		}
 	}
 	return users, nil
@@ -91,11 +87,11 @@ func EnsureSystemAdminInKnownUsers() error {
 	users, _ := LoadKnownUsers()
 	if _, ok := users[SystemAdminID]; !ok {
 		sys := UserRecord{
-			ID:      SystemAdminID,
-			Name:    "Суперадмин",
-			Role:    "system_admin",
+			ID:       SystemAdminID,
+			Name:     "Суперадмин",
+			Role:     "system_admin",
 			Username: "superadmin",
-			Phone:   "",
+			Phone:    "",
 		}
 		return AddKnownUserFull(sys)
 	}
@@ -146,11 +142,11 @@ func LoadPendingUsers() (map[int64]UserRecord, error) {
 		var id int64
 		fmt.Sscanf(parts[0], "%d", &id)
 		users[id] = UserRecord{
-			ID:      id,
-			Name:    parts[1],
+			ID:       id,
+			Name:     parts[1],
 			Username: parts[2],
-			Phone:   parts[3],
+			Phone:    parts[3],
 		}
 	}
 	return users, nil
-} 
+}
